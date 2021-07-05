d07523106efc45692b2928ffa4e5b8180c882a5d
egor123 <egor.lukyanenko@gmail.com>
2021-07-05 20:36:19 +0300

    allowedOrigins

--- frontend/src/views/ChooseATutor.vue
+++ frontend/src/views/ChooseATutor.vue
@@ -28,7 +28,8 @@ export default {
   path: "/choose_a_tutor",
   permisions: {
     roles: "USER",
-    redirect: "LogIn",
+    allowedOrigins: "FindATutor",
+    redirect: "FindATutor",
   },
   components: {
     Background,
