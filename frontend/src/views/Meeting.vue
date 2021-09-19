f61514772923d90d73d01a3b058e3c030b7da945
StopLagginPls <stefanehin4@gmail.com>
2021-09-19 11:48:10 +0300

    Stripe

---
+++ frontend/src/views/Meeting.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="container">
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      meetingID: 5839697402,
+    };
+  },
+
+  //   mounted() {
+  //     console.log("Encoded");
+  //     console.log("Decoded: ");
+  //     let b64 = btoa("Stefan");
+  //     console.log(atob(b64));
+  //   },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/mixins.scss";
+
+#container {
+  @include box-size(100%);
+  @include flexbox();
+  & > * {
+    margin: auto;
+    @include flexbox();
+  }
+}
+</style>
