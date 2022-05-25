ef25e776e28cddb945accb2f58d6abcb8394f847
StopLagginPls <stefanehin4@gmail.com>
2022-05-25 17:03:56 +0300

    vuex index.ts to TS

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,28 +1,33 @@
 import Vue from "vue";
-import Vuex, { Store } from "vuex";
+import Vuex, { Module, Store } from "vuex";
 
 Vue.use(Vuex);
 
 const store: Store<unknown> = new Vuex.Store({});
 export default store;
 
-function init() {
-  const components: __WebpackModuleApi.RequireContext = require.context(
-    "./modules",
-    true,
-    /.js$/
-  );
-
-  const arrOfModules: any[][] = components.keys().map((key) => {
-    const moduleName: string = key.substring(2, key.length - 3),
-      obj: any = components(key).default;
-    return [moduleName, obj];
-  });
-
-  return Object.fromEntries(arrOfModules);
+interface Modules {
+  [index: string]: any;
 }
 
-const modules = init();
+const modules: Modules = (): Modules => {
+  const folder: __WebpackModuleApi.RequireContext = getFolder();
+  return Object.fromEntries(getModulesArr());
+
+  function getFolder(): __WebpackModuleApi.RequireContext {
+    const params: [string, boolean, RegExp] = ["./modules", true, /.js$/];
+    return require.context(...params);
+  }
+  function getModulesArr(): [string, any][] {
+    return folder.keys().map((key) => getKeyValPairArr(key));
+
+    function getKeyValPairArr(key: string): [string, any] {
+      const moduleName: string = key.substring(2, key.length - 3);
+      const module: any = folder(key).default;
+      return [moduleName, module];
+    }
+  }
+};
 
 for (const key in modules) {
   store.registerModule(key, modules[key]);
